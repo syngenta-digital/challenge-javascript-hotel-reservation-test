8fbc113f0c19ad36883dbaff12d265324cfaa4d4
AlbertoDelbone <alberto.delbone_de_faria@syngenta.com>
2022-05-02 17:56:16 -0300

    fix: deleting useless files

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
-function getCheaperHotel (abc) {
-    if (abc == "Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"){
-        return "Lakewood";
-    }else if(abc == "Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"){
-        return "Bridgewood";
-    }else if(abc == "Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"){
-        return "Ridgewood";
-    }
+function getCheaperHotel (input) { //DO NOT change the function's name.
+    return "Cheaper hotel name"
 }
 
-getCheaperHotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)");
 exports.getCheaperHotel = getCheaperHotel
-console.log(123);
